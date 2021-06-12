9c7d769a8110e91fe800bd666a2d3dc327d0dd16
Your Name <you@example.com>
2021-06-12 10:49:12 -0500

    Mejora de vista importar + imagen

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -9,27 +9,32 @@
 @stop
 
 @section('content')
-
-    <form action="{{ route('alumnos.import.excel') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @if ($success == 'true')
-            <div class="form-group p-3 m-0">
-                <p class="alert alert-success col-md-7">{{ $message }}</p>
-            </div>
-        @elseif ($success=="false")
-            <div class="form-group p-3 m-0">
-                <p class="alert alert-danger col-md-7">{{ $message }}</p>
-            </div>
-        @endif
-        <a class="p-3 m-0" target="_blank"
+<main role="main" class="flex-shrink-0">
+    <div class="container">
+        <p><a class="m-0" target="_blank"
             href="https://docs.google.com/spreadsheets/d/1mGPNLLT2_DpFAlmJgkNzKVNQ_t3VrCKDz9EXgsDNRQ4/edit?usp=sharing">Plantilla
-            de Importación</a>
-        <div class="form-group p-3 m-0">
-            <input type="file" name="file" required class="form-control-file">
-        </div>
-        <div class="p-3">
-            <button type="submit" class="btn btn-primary mb-2">Importar</button>
-        </div>
-    </form>
-
+            de Importación</a></p>
+        <form action="{{ route('alumnos.import.excel') }}" method="POST" enctype="multipart/form-data" class="col-12 col-md-12">
+            @csrf
+            @if ($success == 'true')
+                <div class="form-group">
+                    <p class="alert alert-success col-md-7">{{ $message }}</p>
+                </div>
+            @elseif ($success=="false")
+                <div class="form-group">
+                    <p class="alert alert-danger col-md-7">{{ $message }}</p>
+                </div>
+            @endif
+            <div class="form-group pb-3">
+                <input type="file" name="file" required class="form-control-file  col-md-7 p-0">
+            </div>
+            <div class="">
+                <button type="submit" class="btn btn-primary">Importar</button>
+            </div>
+        </form>
+    </div>
+    <div class="p-3 pt-5">
+        <img src="{{asset('/img/formato_importar.png')}}" alt="formato de importacion" class="shadow-lg w-100">
+    </div>
+</main>
 @endsection
